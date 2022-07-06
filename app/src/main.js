fbcbe6d8eb259cd0ded058fe12ba17d4ab58da6d
Saveliy <svlvslvch@gmail.com>
2022-07-06 16:53:07 +0300

    Adding React. Deleting files

--- app/src/main.js
+++ app/src/main.js
@@ -1,25 +1,32 @@
 
-import $ from 'jquery';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Editor from './Components/editor';
 
-function getPageList() {
-  $("h1").remove();
-  $.get("./api", data => {
-    data.forEach(file => {
-      $("body").append(`<h1>${file}</h1>`)
-    })
-  }, "JSON")  
-}
-
-getPageList();
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <Editor />
+);
 
 
-$("button").click(() => {
-  $.post("./api/createNewPage.php", {
-    "name" : $("input").val()
-  }, () => {
-    getPageList();
-  })
-  .fail(() => {
-    alert('Страница уже существует')
-  })
-})
+// function getPageList() {
+//   $("h1").remove();
+//   $.get("./api", data => {
+//     data.forEach(file => {
+//       $("body").append(`<h1>${file}</h1>`)
+//     })
+//   }, "JSON")  
+// }
+
+// getPageList();
+
+// $("button").click(() => {
+//   $.post("./api/createNewPage.php", {
+//     "name" : $("input").val()
+//   }, () => {
+//     getPageList();
+//   })
+//   .fail(() => {
+//     alert('Страница уже существует')
+//   })
+// })
