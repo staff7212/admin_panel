e9c356ecc7b83a88482a82568c12d1487a7ad342
Saveliy <svlvslvch@gmail.com>
2022-07-10 14:22:22 +0300

    Work optimization

--- app/src/Components/editor/editor.js
+++ app/src/Components/editor/editor.js
@@ -3,6 +3,9 @@ import "../../helpers/iframeLoader.js";
 import axios from 'axios';
 import React, {Component} from 'react';
 
+import DOMHelper from '../../helpers/dom-helper';
+import EditorText from '../editor-text';
+
 export default class Editor extends Component {
   constructor() {
     super();
@@ -28,81 +31,52 @@ export default class Editor extends Component {
 
     axios
       .get(`../${page}?rnd=${Math.random()}`)
-      .then(res => this.parseStrToDON(res.data))
-      .then(this.wrapTextNodes)
+      .then(res => DOMHelper.parseStrToDOM(res.data))
+      .then(DOMHelper.wrapTextNodes)
       .then(dom => {
         this.virtualDom = dom;
         return dom
       })
-      .then(this.serializeDOMToString)
+      .then(DOMHelper.serializeDOMToString)
       .then(html => axios.post("./api/saveTempPage.php", {html}))
       .then(() => this.iframe.load("../temp.html"))
       .then(() => this.enableEditing())
+      .then(() => this.injectStyles());
   }
 
   save() {
     const newDom = this.virtualDom.cloneNode(this.virtualDom);
-    this.unwrapTextNodes(newDom);
-    const html = this.serializeDOMToString(newDom);
+    DOMHelper.unwrapTextNodes(newDom);
+    const html = DOMHelper.serializeDOMToString(newDom);
     axios
       .post("./api/savePage.php", {pageName: this.currentPage, html})
   }
 
   enableEditing() {
     this.iframe.contentDocument.body.querySelectorAll("text-editor").forEach(element => {
-      element.contentEditable = "true";
-      element.addEventListener('input', () => {
-        this.onTextEdit(element);
-      })
+
+      const id = element.getAttribute("nodeid");
+      const virtualElement = this.virtualDom.body.querySelector(`[nodeid="${id}"]`)
+
+      new EditorText(element, virtualElement);
     })
   }
 
-  onTextEdit(element) {
-    const id = element.getAttribute("nodeid");
-    this.virtualDom.body.querySelector(`[nodeid="${id}"]`).innerHTML = element.innerHTML;
+  injectStyles() {
+    const style =  this.iframe.contentDocument.createElement("style");
+    style.innerHTML = `
+      text-editor:hover {
+        outline: 2px solid orange;
+        outline-offser: 8px;
+      }
+      text-editor:focus {
+        outline: 2px solid red;
+        outline-offser: 8px;
+      }
+    `;
+    this.iframe.contentDocument.head.appendChild(style);
   }
 
-  parseStrToDON(str) {
-    const parser = new DOMParser();
-    return parser.parseFromString(str, "text/html")
-  }
-
-  wrapTextNodes(dom) {
-    const body = dom.body;
-    let textNodes = []
-
-    function recursy(element) {
-      element.childNodes.forEach(node => {
-        if(node.nodeName === "#text" && node.nodeValue.replace(/\s+/g, "").length > 0) {
-          textNodes.push(node);
-        } else {
-          recursy(node);
-        }
-      })
-    }
-
-    recursy(body);
-
-    textNodes.forEach((node, i) => {
-      const wrapper = dom.createElement('text-editor');
-      node.parentNode.replaceChild(wrapper, node);
-      wrapper.appendChild(node);
-      wrapper.setAttribute("nodeid", i)
-    });
-
-    return dom
-  }
-
-  serializeDOMToString(dom) {
-    const serializer = new XMLSerializer();
-    return serializer.serializeToString(dom); 
-  }
-
-  unwrapTextNodes(dom) {
-    dom.body.querySelectorAll("text-editor").forEach(element => {
-      element.parentNode.replaceChild(element.firstChild, element);
-    })
-  }
 
   loadPageList() {
     axios
